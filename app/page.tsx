c92af48f6ff160a3fad4018729f13b74f4ac7457
Hesam <farsidehnavih@gmail.com>
2026-01-03 12:24:33 +0330

    1.1.0

--- app/page.tsx
+++ app/page.tsx
@@ -50,44 +50,11 @@ export default function Home() {
       <div className={style.DarkPage}>
         <div className={style.FirstRow}>
           <img className={style.Img} src={"/img/brain.svg"} alt="" />
-          <div className={style.FirstRowTextSection}>
-            <p className={style.WTitle}>آینده هوش مصنوعی، فقط با یک کلیک</p>
-            <p className={style.WSubtitle}>
-              ClickPlus دسترسی سریع و ساده به پیشرفته‌ترین ابزارهای هوش مصنوعی
-              متن و تصویر — بدون دردسر، بدون محدودیت
-            </p>
-            <p className={style.Text}>
-              هر آنچه یک حرفه ای برای زندگی نیاز دارد را یکجا جمع کرده ایم تا با
-              یک کلیک به آن دسترسی داشته باشید.
-            </p>
-            <div className={style.StartRow}>
-              <button className={style.BuyButton}>دیدن پلن‌ها</button>
-              <button className={style.StartButton}>
-                <p className={style.ButtonText}>شروع رایگان</p>
-                <img
-                  className={style.ArrowIcon}
-                  src={"./icon/Arrow.svg"}
-                  alt=""
-                />
-              </button>
-            </div>
-            <div className={style.Icons}>
-              <div className={style.IconItem}>
-                <img
-                  className={style.IconImg}
-                  src="/model-icons/ChatGPT.svg"
-                  alt=""
-                />
-                <p className={style.IconText}>ChatGPT</p>
-              </div>
-              <div className={style.IconItem}>
-                <img
-                  className={style.IconImg}
-                  src="/model-icons/Midjourney.svg"
-                  alt=""
-                />
-                <p className={style.IconText}>Midjourney</p>
-              </div>
+          <div className={style.Newspaper}>
+            <div className={style.NTitle}>به مجله ی خبری ما بپیوندید</div>
+            <div className={style.JoinRow}>
+              <input type="text" className={style.Inp} placeholder="ایمیل" />
+              <button className={style.Join}>پیوستن</button>
             </div>
           </div>
         </div>
