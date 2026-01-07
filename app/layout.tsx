87eafe3fec87816efd8c6ac1066159003120a9cb
Hesam <farsidehnavih@gmail.com>
2026-01-07 14:41:30 +0330

    1.0.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { type Metadata } from "next";
-import { faIR } from '@clerk/localizations'
+// import { faIR } from '@clerk/localizations'
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -21,16 +21,16 @@ export const metadata: Metadata = {
   icons: ["/Logo/Tiny.svg"],
 };
 
-import { ClerkProvider } from '@clerk/nextjs'
+// import { ClerkProvider } from '@clerk/nextjs'
 export default function RootLayout({ children }:{children:React.ReactNode}) {
   return (
-    <ClerkProvider localization={faIR}>
+    // <ClerkProvider localization={faIR}>
       <html>
         <body>
           <Header />
           {children}
         </body>
       </html>
-    </ClerkProvider>
+    // </ClerkProvider>
   )
 }
